de715884af22d12fe984716f81978106ea583d23
webmasterpf-DG <webmaster@provence-formation.com>
2011-07-18 16:42:45 +0200

    Début mise en place PROD
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

---
+++ Construction/index.php
@@ -0,0 +1,54 @@
+<?php
+/* 
+ * Page attente sud formation
+ * A retirer quand Wordpress installé
+ */
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>Site Sud Formation en construction</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style type="text/css">
+body{
+            background: #F1F1F1;
+            margin: 50px;
+}
+#page{
+    background: none repeat scroll 0 0 #FFFFFF;
+    box-shadow: 0 0 5px #656565;
+    margin: auto;
+    padding: 0 20px;
+    width: 900px;
+
+}
+.texte{
+    padding: 10px;
+}
+
+
+    </style>
+  </head>
+  <body>
+      <div id="page">
+
+          <div class="texte">
+              <h1>Bienvenue sur le site de Sud Formation</h1>
+              <p>
+                  Le site Sud Formation est cours de r&eacute;alisation.Il proposera l'actualit&eacute; de l'association et des informations sur ses membres.
+
+              </p>
+              <ul>
+                  <li>Association M&eacute;dico-sociale de Provence</li>
+                  <li>Formation et M&eacute;tier</li>
+                  <li>ISM Cadenelle</li>
+                  <li>Provence Formation</li>
+
+              </ul>
+
+          </div>
+
+      </div>
+  </body>
+</html>
